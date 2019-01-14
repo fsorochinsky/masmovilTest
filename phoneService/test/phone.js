a893a880e36965c5d33706cc1f30fbbd7d8d81eb
fsorochynskyi <fsorochynskyi@n-ix.com>
2019-01-14 13:58:22 +0200

    update phone tests

--- phoneService/test/phone.js
+++ phoneService/test/phone.js
@@ -5,14 +5,21 @@ const models = require('../models');
 const phoneCtrl = require('../controllers/phone');
 const {
   req,
-  successResponse,
-  tokenErrorResponse
+  phoneList,
+  updatedItemsResponse,
+  tokenErrorResponse,
+  wrongItemCountReq,
+  wrongPhoneIdReq,
+  wrongPhoneIdErrorResponse,
+  wrongItemCountErrorResponse
 } = require('../testData/phone');
 
 const wrongRequest = {body:{}};
 wrongRequest.body = Object.assign({}, req.body);
 
 req.body.token = require('../config/config').token;
+wrongItemCountReq.body.token = require('../config/config').token;
+wrongPhoneIdReq.body.token = require('../config/config').token;
 
 describe('check update phones count', () => {
   let stubs;
@@ -21,6 +28,9 @@ describe('check update phones count', () => {
     stubs = [
       sinon.stub(models.phone, 'updateItemsCount').callsFake(() => {
         return Promise.resolve();
+      }),
+      sinon.stub(models.phone, 'findAll').callsFake(() => {
+        return Promise.resolve(phoneList);
       })
     ];
   });
@@ -34,16 +44,31 @@ describe('check update phones count', () => {
 
   it('should return success', () => {
     return phoneCtrl.updateCounts(req).then((response) => {
-      return expect(response).to.be.equal(successResponse);
+      return expect(response).to.deep.equal(updatedItemsResponse);
     })
   });
 
-  it('should return error token error', () => {
-    return phoneCtrl.updateCounts(wrongRequest).then((response) => {
-      return Promise.reject('should return token error error');
+  it('should return token error', () => {
+    return phoneCtrl.updateCounts(wrongRequest).then(() => {
+      return Promise.reject('should return token error');
     }).catch((err) => {
       return expect(err).to.deep.equal(tokenErrorResponse);
     })
   });
 
+  it('should return wrong item count error', () => {
+    return phoneCtrl.updateCounts(wrongItemCountReq).then(() => {
+      return Promise.reject('should return wrong item count error');
+    }).catch((err) => {
+      return expect(err).to.deep.equal(wrongItemCountErrorResponse);
+    })
+  });
+
+  it('should return wrong phone id error', () => {
+    return phoneCtrl.updateCounts(wrongPhoneIdReq).then(() => {
+      return Promise.reject('should return wrong phone id error');
+    }).catch((err) => {
+      return expect(err).to.deep.equal(wrongPhoneIdErrorResponse);
+    })
+  });
 });
